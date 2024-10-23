22de7df432bec44856721b6ac7a542bf3de89c0c
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-10-24 00:30:40 +0600

    Base_url updated for frontend

--- myportfolio/src/service/Api.tsx
+++ myportfolio/src/service/Api.tsx
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://myportfolioapi-ebon.vercel.app/api";
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "https://myportfolio-yqp9.onrender.com/api";
+// export const BASE_URL = "http://localhost:8000/api";
