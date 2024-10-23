d860e3ce9c4d7e3fb40a94c82abbd1fbb35c4947
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-10-23 23:29:09 +0600

    updated sqllite

--- core/core/settings.py
+++ core/core/settings.py
@@ -88,26 +88,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'verceldb',
-        'USER': 'default',
-        'PASSWORD': '51spiRCAzTLX',
-        'HOST': 'ep-red-waterfall-a4ezd37u-pooler.us-east-1.aws.neon.tech',
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'verceldb',
+#         'USER': 'default',
+#         'PASSWORD': '51spiRCAzTLX',
+#         'HOST': 'ep-red-waterfall-a4ezd37u-pooler.us-east-1.aws.neon.tech',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'sslmode': 'require',
+#         },
+#     }
+#}
 
 # POSTGRES_URL="postgres://default:51spiRCAzTLX@ep-red-waterfall-a4ezd37u-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
 # POSTGRES_PRISMA_URL="postgres://default:51spiRCAzTLX@ep-red-waterfall-a4ezd37u-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
@@ -155,7 +155,7 @@ import os
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
