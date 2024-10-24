09ef60a55398fc51e0c583fbce20dbbd2069b33d
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-10-24 22:47:27 +0600

    frontend api link added

--- myportfolio/src/service/Api.tsx
+++ myportfolio/src/service/Api.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://myportfolio-yqp9.onrender.com/api";
+export const BASE_URL = "https://myportfolio-4s88.vercel.app/api";
 // export const BASE_URL = "http://localhost:8000/api";
